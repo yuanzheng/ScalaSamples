c49cfcfe832c8028b25b3ad42de9bf1c8e7ecc40
Lucas Song <ysong@insidesales.com>
2015-08-27 02:06:17 -0600

    learned value definitions

---
+++ src/test/scala/learning/datatypes/typeLearningTest.scala
@@ -0,0 +1,25 @@
+package learning.datatypes
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class typeLearningTest extends FlatSpec with Matchers {
+  
+  
+  "typeLearning" should "show value definitions" in {
+    typeLearning.change = 100
+    
+    typeLearning.byName should be (102)
+    typeLearning.byValue should be (1)
+    println(s"by name: 2+change = ${typeLearning.byName}")
+    println(s"by value: 1+ change = ${typeLearning.byValue}")
+    
+    typeLearning.change = 1000
+    
+    typeLearning.byName should be (1002)
+    typeLearning.byValue should be (1)
+    println(s"by name: 2+change = ${typeLearning.byName}")
+    println(s"by value: 1+ change = ${typeLearning.byValue}")
+  }
+
+}
